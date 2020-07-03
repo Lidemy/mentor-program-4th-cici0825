813bbc87c11923a20e7b4061369cfe1c4712eba1
wufangci <cicichen0825@gmail.com>
2020-07-04 03:49:08 +0800

    week02

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,23 @@
 function join(arr, concatStr) {
-  
+
+    var result = ''
+
+    for (var i = 0; i < arr.length; i++) {
+        result += arr[i] + concatStr
+    }
+
+    return result
+
 }
 
 function repeat(str, times) {
-  
+
+    var result = ''
+    for (var i = 0; i < times; i++) {
+        result += str
+    }
+    return result
+
 }
 
 console.log(join(['a'], '!'));
